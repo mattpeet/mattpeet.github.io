71d553d2ee2f45226cee49b688bc5cae9fc8398a
suzanne <suzanne.aitchison@gmail.com>
2018-11-11 17:28:30 +0000

    adds header elements

--- src/js/components/container/Header.js
+++ src/js/components/container/Header.js
@@ -16,8 +16,21 @@ class Header extends Component {
 
   render() {
     return (
-      <div className="header-container">
-        <p>header content</p>
+      <div className="header-container col-md-12">
+        <div className="row">
+          <h1 id="illustrator-name" className="col-md-6">MATT PEET</h1>
+        </div>
+        <div className="row">
+          <h3 id="header-subtitle" className="col-md-6">illustration</h3>
+        </div>
+        <div className="row social-and-nav">
+          <i className="fab fa-instagram col-md-1"></i>
+          <i className="fab fa-twitter col-md-1"></i>
+          <i className="fab fa-tumblr-square col-md-1"></i>
+          <i className="fab fa-facebook-square col-md-1"></i>
+          <i className="fas fa-envelope col-md-1"></i>
+          <i id="hamburger-menu" className="fas fa-bars col-md-1"></i>
+        </div>
       </div>
     );
   }
