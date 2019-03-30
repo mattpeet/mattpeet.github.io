2046ef574d871a1c7339a089605700a7ef844209
mattpeet <matt@mattpeetillustration.com>
2019-03-30 11:16:49 +0000

    add closeable inner portion

--- src/js/components/container/MenuOverlay.js
+++ src/js/components/container/MenuOverlay.js
@@ -22,23 +22,24 @@ class MenuOverlay extends Component {
     console.log("close")
     this.props.toggleMenu()
   }
-
   render() {
     return (
       <div id="menu-overlay" style={this.getStyle()}>
-        <div className="menu-inner">
-        <div id="project-filters">
-          <ul>
-            <Link to="/"><li onClick={this.closeMenu}>projects</li></Link>
-           
-          </ul>
+        <div className="closeable-inner-menu" onClick={this.props.toggleMenu}>
         </div>
-        <div id="sundry-nav">
-          <ul>
-          <Link to="/about"><li onClick={this.closeMenu}>about</li></Link>
-          <Link to="/contact"><li onClick={this.closeMenu}>contact</li></Link>
-          </ul>
-        </div>
+        <div className="menu-inner" >
+          <div id="project-filters">
+            <ul>
+              <Link to="/"><li onClick={this.closeMenu}>projects</li></Link>
+             
+            </ul>
+          </div>
+          <div id="sundry-nav">
+            <ul>
+            <Link to="/about"><li onClick={this.closeMenu}>about</li></Link>
+            <Link to="/contact"><li onClick={this.closeMenu}>contact</li></Link>
+            </ul>
+          </div>
         </div>
       </div>
       )
